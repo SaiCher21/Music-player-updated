56bf4980528299661669870ddbf8d6654962143c
krischer89 <cherukurikrishnakishore@gmail.com>
2023-11-03 20:13:06 +0530

    changes made

---
+++ assets/js/music-list.js
@@ -0,0 +1,50 @@
+let allMusic = [
+    {
+        name:"Living Hell",
+        artist:"Bella poarch",
+        song:"music-1",
+        src:"music-1"
+    },
+    {
+        name:"Leviating",
+        artist:"Dua lipa",
+        song:"music-2",
+        src:"music-2"
+    },
+    {
+        name:"Dynamite",
+        artist:"BTS",
+        song:"music-3",
+        src:"music-3"
+    },
+    {
+        name:"Dolls",
+        artist:"Bella poarch ",
+        song:"music-4",
+        src:"music-4"
+    },
+    {
+        name:"We don't any more",
+        artist:"Charlie puth, Selena Gomez",
+        song:"music-5",
+        src:"music-5"
+    },
+    {
+        name:"Shape of you",
+        artist:"Ed sheeran",
+        song:"music-6",
+        src:"music-6"
+    },
+    {
+        name:"See you again",
+        artist:"charlie puth, Wiz Khalifa",
+        song:"music-7",
+        src:"music-7"
+    },
+    {
+        name:"People",
+        artist:"Libianca, Omah Lay",
+        song:"music-8",
+        src:"music-8"
+    }
+];
