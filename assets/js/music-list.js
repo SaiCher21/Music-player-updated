639c8c9da2895c661c03906fb045368bf86bba03
krischer89 <cherukurikrishnakishore@gmail.com>
2023-11-04 17:49:43 +0530

    artist

--- assets/js/music-list.js
+++ assets/js/music-list.js
@@ -1,49 +1,49 @@
 let allMusic = [
     {
-        name:"Living Hell",
-        artist:"Bella poarch",
+        name:"Tokyo",
+        artist:"unknown",
         song:"music-1",
         src:"music-1"
     },
     {
-        name:"Leviating",
-        artist:"Dua lipa",
+        name:"Go Go Go",
+        artist:"unknown",
         song:"music-2",
         src:"music-2"
     },
     {
-        name:"Dynamite",
-        artist:"BTS",
+        name:"soul searching",
+        artist:"unknown",
         song:"music-3",
         src:"music-3"
     },
     {
-        name:"Dolls",
-        artist:"Bella poarch ",
+        name:"Monogram",
+        artist:"unknown  ",
         song:"music-4",
         src:"music-4"
     },
     {
-        name:"We don't any more",
-        artist:"Charlie puth, Selena Gomez",
+        name:"stranger in a way",
+        artist:"unknown",
         song:"music-5",
         src:"music-5"
     },
     {
-        name:"Shape of you",
-        artist:"Ed sheeran",
+        name:"Electro fight",
+        artist:"unknown",
         song:"music-6",
         src:"music-6"
     },
     {
-        name:"See you again",
-        artist:"charlie puth, Wiz Khalifa",
+        name:"Arcade",
+        artist:"cunknown",
         song:"music-7",
         src:"music-7"
     },
     {
         name:"People",
-        artist:"Libianca, Omah Lay",
+        artist:"unknown",
         song:"music-8",
         src:"music-8"
     }
